5fcc5af51a4dc22d058d8d725fdee322439e09cd
aandersonruth2-code <aandersonruth2@gmail.com>
2025-12-14 15:58:12 +0100

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // ✅ important for Vercel deployment
-});
+  base: './', // Important for correct asset paths
+})
